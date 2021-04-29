2e58658796337b103cbc73af5dee67d126d93aa8
Reantsiko <fabien.abbenbroek@gmail.com>
2021-04-29 15:01:18 +0200

    Started project for a stream game to be played with commands on twitch, setting up bot, can read and send messages in chat, can receive whispers and read them

---
+++ TwitchRPG/Assets/Secrets.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secrets
+{
+    public static string client_id = "wr2vyhv6fmdv2ewn9g5ciifm6gi8cn";
+    public static string client_secret = "b7a1ihgbfwku73dw7u4awosktj5eez";
+    public static string bot_access_token = "lkqzeerehb5ruja54urrswl142nyn9";
+    public static string bot_refresh_token = "edgnc4r6m20n7igmt0yr3pv7j6zg1yd5rqswstgg77cuszsx9t";
+}
---
+++ TwitchRPG/Assets/TwitchClient.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwitchLib.Client.Models;
+using TwitchLib.Unity;
+
+public class TwitchClient : MonoBehaviour
+{
+    public Client client;
+    private string channel_name = "reantsikox";
+    [SerializeField] private GameObject capsulePrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.runInBackground = true;
+        ConnectionCredentials credentials = new ConnectionCredentials("twitchsurvivalrpg", Secrets.bot_access_token);
+        client = new Client();
+        client.Initialize(credentials, channel_name);
+
+        client.OnMessageReceived += Client_OnMessageReceived;
+        client.OnWhisperReceived += Client_OnWhisperReceived;
+        client.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            client.SendMessage(client.JoinedChannels[0], $"This is a test message from the bot!");
+    }
+
+    private void Client_OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+    {
+        Debug.Log($"The bot just read a message in chat");
+        Debug.Log($"{sender.ToString()}");
+        Debug.Log($"{e.ChatMessage.Username} said {e.ChatMessage.Message}");
+        ParseMessage(e.ChatMessage.Message);
+    }
+
+    private void Client_OnWhisperReceived(object sender, TwitchLib.Client.Events.OnWhisperReceivedArgs e)
+    {
+        ParseMessage(e.WhisperMessage.Message);
+    }
+
+    private void ParseMessage(string message)
+    {
+        var splitMessage = message.Split(' ');
+        if (splitMessage.Length > 0 && !string.IsNullOrEmpty(splitMessage[0]) && splitMessage[0][0] == '!')
+        {
+            if (string.Compare(splitMessage[0], "!create") == 0)
+            {
+                if (capsulePrefab != null)
+                {
+                    Instantiate(capsulePrefab, new Vector3(Random.Range(-5f, 5f), 1f, Random.Range(-7f, 23f)), Quaternion.identity);
+                    client.SendMessage(client.JoinedChannels[0], $"Created and Spawned a new capsule!");
+                }
+            }
+        }
+        else
+            client.SendMessage(client.JoinedChannels[0], $"Does not compute!");
+    }
+}
